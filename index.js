4bf6ebdc00a36a88b67ca765dee0bd85c7433773
mystical-process-8698 <andrew.j.ribas@gmail.com>
2020-04-24 19:05:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,3 +30,13 @@ function prependKitten(name)
 {
   return [name, ...kittens]
 }
+
+function removeLastKitten()
+{
+  return kittens.slice(1)
+}
+
+function removeFirstKitten()
+{
+  return kittens.slice(1)
+}
